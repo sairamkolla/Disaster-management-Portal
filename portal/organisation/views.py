db52f530b0c5a304976ba76e98d82e331d6b9b47
sairam kolla <sai.kolla24@gmail.com>
2015-09-28 02:29:24 +0530

    views for organisation added

--- portal/organisation/views.py
+++ portal/organisation/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from organisation.models import Notifications_Org
 # Create your views here.
+
+def org_home(request,org_id):
+    notification=Notifications_Org.objects.all() 
+    return render_to_response('org_home.html')
