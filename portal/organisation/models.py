079f4244b030a82770587def6734e7c6bb44d4b1
sairam kolla <sai.kolla24@gmail.com>
2015-09-28 00:14:35 +0530

    modelschanging

--- portal/organisation/models.py
+++ portal/organisation/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Orgs(models.Model):
+    person_in_charge=models.CharField(max_length=100)
+    people_working=models.IntegerField(default=0)
+    conta
