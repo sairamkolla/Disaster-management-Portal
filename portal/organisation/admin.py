a3d7289189ac6ae71e872407833042cb2cd2394b
sairam kolla <sai.kolla24@gmail.com>
2015-09-28 02:28:53 +0530

    Normal Commit

--- portal/organisation/admin.py
+++ portal/organisation/admin.py
@@ -3,7 +3,7 @@ from organisation.models import *
 # Register your models here.
 
 admin.site.register(Orgs)
-admin.site.register(Contact_Numbers_orgs)
+admin.site.register(Contact_Numbers_Orgs)
 admin.site.register(Contact_Mails_Orgs)
 admin.site.register(Name_Orgs)
 admin.site.register(Address_Org)
