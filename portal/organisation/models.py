c7d37363fd3d8457a7c516610890e8f7e838d5de
sairam kolla <sai.kolla24@gmail.com>
2015-09-28 01:12:29 +0530

    Org models created

--- portal/organisation/models.py
+++ portal/organisation/models.py
@@ -5,4 +5,48 @@ from django.db import models
 class Orgs(models.Model):
     person_in_charge=models.CharField(max_length=100)
     people_working=models.IntegerField(default=0)
-    conta
+    name_of_org=models.CharField(max_length=100)
+    latitude=models.CharField(max_length=15)
+    longitude=models.CharField(max_length=15)
+
+
+class Contact_Numbers_Orgs(models.Model):
+    org_id=models.ForeignKey(Orgs)
+    contact_number=models.CharField(max_length=12)
+
+class Contact_Mails_Orgs(models.Model):
+    org_id=models.ForeignKey(Orgs)
+    contact_mail=models.CharField(max_length=50)
+
+class Name_Orgs(models.Model):
+    org_id=models.ForeignKey(Orgs)
+    name=models.CharField(max_length=100)
+
+class Address_Org(models.Model):
+    org_id=models.ForeignKey(Orgs)
+    street_name=models.CharField(max_length=50)
+    state_name=models.CharFiled(max_length=20)
+    door_number=models.CharField(max_length=10)
+    city_name=models.CharField(max_length=20)
+    pincode=models.CharField(max_length=6)
+
+class Messages_Orgs(models.Model):
+    sender_org_id=models.ForeignKey(Orgs)
+    receiver_org_id=models.ForeignKey(Orgs)
+    message_content=models.charField(max_length=256)
+
+class Messages_From_Admin(models.Model):
+    target_org_id=models.ForeignKey(Orgs)
+    message_content=models.CharField(max_length=500)
+
+class Notifications_Org(models.Model):
+    target_org_id=models.ForeginKey(Orgs)
+    is_message_from_org=models.BooleanField(default=0)
+    message_from_org_id=models.ForeignKey(Messages_Orgs)
+    is_message_from admin=models.BooleanField(default=0)
+    message_from_admin_id=models.Foreignkey(Messages_From_Admin)
+    is_request_from_admin=models.BooleanField(default=0)
+    disaster_id=models.IntegerField()     ######################################  should make disasters app
+    created=models.DateTimeField(auto_now_add=True)
+
+
