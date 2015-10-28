9a9e11133d27e6f8fb647349486c9ee62b973862
sairam kolla <sai.kolla24@gmail.com>
2015-10-29 00:39:13 +0530

    Organisation models adjusted for login

--- portal/organisation/models.py
+++ portal/organisation/models.py
@@ -1,36 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 #from django.conf import settings
 # Create your models here.
 
 class Orgs(models.Model):
-    person_in_charge=models.CharField(max_length=100)
-    people_working=models.IntegerField(default=0)
+    user=models.ForeignKey(User,null=True)
+    org_head=models.CharField(max_length=100)
+    org_strength=models.IntegerField(default=0)
     name_of_org=models.CharField(max_length=100)
     latitude=models.CharField(max_length=15)
     longitude=models.CharField(max_length=15)
     created=models.DateTimeField(auto_now_add=True)
-    tags=models.CharField(max_length=100)
+    tags=models.CharField(max_length=100,null=True)
     def __unicode__(self):
-        #return unicode(self.name_of_org)
         return unicode(self.name_of_org)
 
 
 class Contact_Numbers_Orgs(models.Model):
-    org_id=models.ForeignKey(Orgs)
+    org_id=models.ForeignKey(Orgs,null=True)
     contact_number=models.CharField(max_length=12)
     created=models.DateTimeField(auto_now_add=True)
 
 class Contact_Mails_Orgs(models.Model):
-    org_id=models.ForeignKey(Orgs)
+    org_id=models.ForeignKey(Orgs,null=True)
     contact_mail=models.CharField(max_length=50)
     created=models.DateTimeField(auto_now_add=True)
 
-#class Name_Orgs(models.Model):
-#    org_id=models.ForeignKey(Orgs)
-#    name=models.CharField(max_length=100)
-
 class Address_Org(models.Model):
-    org_id=models.ForeignKey(Orgs)
+    org_id=models.ForeignKey(Orgs,null=True)
     street_name=models.CharField(max_length=50)
     state_name=models.CharField(max_length=20)
     door_number=models.CharField(max_length=10)
@@ -43,8 +40,8 @@ class Address_Org(models.Model):
 
 
 class Messages_Orgs(models.Model):
-    sender_org_id=models.ForeignKey(Orgs,related_name="sender")
-    receiver_org_id=models.ForeignKey(Orgs,related_name="receiver")
+    sender_org_id=models.ForeignKey(Orgs,related_name="sender",null=True)
+    receiver_org_id=models.ForeignKey(Orgs,related_name="receiver",null=True)
     message_content=models.CharField(max_length=256)
     created=models.DateTimeField(auto_now_add=True)
 
@@ -52,7 +49,7 @@ class Messages_Orgs(models.Model):
         return unicode(self.message_content)
 
 class Messages_From_Admin(models.Model):
-    target_org_id=models.ForeignKey(Orgs)
+    target_org_id=models.ForeignKey(Orgs,null=True)
     message_content=models.CharField(max_length=500)
     created=models.DateTimeField(auto_now_add=True)
 
@@ -60,7 +57,7 @@ class Messages_From_Admin(models.Model):
         return unicode(self.id)
 
 class Notifications_Org(models.Model):
-    target_org_id=models.ForeignKey(Orgs)
+    target_org_id=models.ForeignKey(Orgs,null=True)
     is_message_from_org=models.BooleanField(default=0)
     message_from_org_id=models.ForeignKey(Messages_Orgs,null=True,blank=True)
     is_message_from_admin=models.BooleanField(default=0)
--- portal/organisation/views.py
+++ portal/organisation/views.py
@@ -8,27 +8,32 @@ from django.http import HttpResponseRedirect
 from siteadmin.models import Acceptance_Disaster_Org, Disaster_Description
 # Create your views here.
 
-def org_home(request,org_id):
-    notifications=Notifications_Org.objects.filter(target_org_id=Orgs.objects.get(id=org_id))
-    present_org=Orgs.objects.get(id=org_id)
+#def org_home(request,org_id):
+def org_home(request):
+    #notifications=Notifications_Org.objects.filter(target_org_id=Orgs.objects.get(id=org_id))
+    notifications=Notifications_Org.objects.filter(target_org_id=Orgs.objects.get(user=request.user))
+    #present_org=Orgs.objects.get(id=org_id)
+    present_org=Orgs.objects.get(user=request.user)
     args={}
     disasters=Disaster_Description.objects.all()
     args.update(csrf(request))
-    messages=Messages_Orgs.objects.filter(receiver_org_id=Orgs.objects.get(id=org_id))
+    #messages=Messages_Orgs.objects.filter(receiver_org_id=Orgs.objects.get(id=org_id))
+    messages=Messages_Orgs.objects.filter(receiver_org_id=Orgs.objects.get(user=request.user))
     args['form']=Messageform()
     args['messages']=messages
     args['disasters']=disasters
     args['notifications']=notifications
-    args['org_id']=org_id
     args['present_org']=present_org
     return render_to_response('organisation/maintrail.html',args)
 
-def create_message_org(request,org_id):
+#def create_message_org(request,org_id):
+def create_message_org(request):
     if request.POST:
         form=Messageform(request.POST)
         if form.is_valid:
             a=form.save(commit=False)
-            a.sender_org_id=Orgs.objects.get(id=org_id)
+            #a.sender_org_id=Orgs.objects.get(id=org_id)
+            a.sender_org_id=Orgs.objects.get(user=request.user)
             a.save()
             ##### Message is stored into database now #####
             send=a.sender_org_id
@@ -37,13 +42,18 @@ def create_message_org(request,org_id):
             mm=Notifications_Org(target_org_id=receive,is_message_from_org=1,message_from_org_id=a)
             ##### A notification for the receiver is  created now #####
             mm.save()
-            return HttpResponseRedirect('/orgs/get/%s' % org_id)
-def profile(request,org_id):
-    org=Orgs.objects.get(id=org_id)
+            return HttpResponseRedirect('/')
+            #return HttpResponseRedirect('/orgs/get/%s' % org_id)
+def profile(request):
+    #org=Orgs.objects.get(id=org_id)
+    org=Orgs.objects.get(user=request.user)
     args={}
     args['org']=org
-    contact_numbers=Contact_Numbers_Orgs.objects.filter(org_id=Orgs.objects.get(id=org_id))
+    #contact_numbers=Contact_Numbers_Orgs.objects.filter(org_id=Orgs.objects.get(id=org_id))
+    contact_numbers=Contact_Numbers_Orgs.objects.filter(org_id=Orgs.objects.get(user=request.user))
     args['contact_numbers']=contact_numbers
-    contact_mails=Contact_Mails_Orgs.objects.filter(org_id=Orgs.objects.get(id=org_id))
+    #contact_mails=Contact_Mails_Orgs.objects.filter(org_id=Orgs.objects.get(id=org_id))
+    contact_mails=Contact_Mails_Orgs.objects.filter(org_id=Orgs.objects.get(user=request.user))
     args['contact_mails']=contact_mails
+    args['username']=request.user.username
     return render_to_response('organisation/orgprofiletrail.html',args)
