94cf0a662a9d223bb8489f915a7a0d23cb3f1829
sairam kolla <sai.kolla24@gmail.com>
2015-11-16 01:34:32 +0530

    serializers updated

--- portal/data/serializers.py
+++ portal/data/serializers.py
@@ -1,33 +1,40 @@
 from rest_framework import serializers
-from organisation.models import *
-from siteadmin.models import *
+from org_models import *
+from siteadmin_models import *
 
-### Organisation related serializers start here
+
+# Organisation related serializers start here
 
 class OrgProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orgs
-        fields= ("userid","org_head","org_strength","name_of_org","latitude","longitude","tags")
+        fields = ("user", "org_head", "org_strength", "name_of_org", "latitude", "longitude", "tags")
 
 
 class OrgListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orgs
-        fields= ("id","name_of_org")
+        fields = ("id", "name_of_org")
+
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Messages_Orgs
-        fields = ("id","sender_org_id","message_content","created")
+        model = MessagesOrgs
+        fields = ("id", "sender_org", "message_content", "created")
 
 
-###### Admin related serializers start here
+# Admin related serializers start here
 
 class DisasterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Disaster_Description
-        fields = ("id","created","disaster_name","disaster_code","reason","no_people_affected","latitude","longitude")
+        model = DisasterDescription
+        fields = ("id", "created", "disaster_name", "disaster_code", "reason",
+                  "no_people_affected", "latitude", "longitude"
+                  )
+
+
 class ProposalSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Disaster_Description
-        fields = ("id","created","disaster_name","disaster_code","reason","no_people_affected","latitude","longitude")
+        model = DisasterDescription
+        fields = (
+            "id", "created", "disaster_name", "disaster_code", "reason", "no_people_affected", "latitude", "longitude")
